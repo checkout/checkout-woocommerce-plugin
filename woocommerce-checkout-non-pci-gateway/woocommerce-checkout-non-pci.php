3e633abdb9ea708d847fa9fd5b9f9d7a983037e1
nicolas-maalouf-cko <nicolas.maalouf@checkout.com>
2017-01-12 12:46:13 +0000

    Add optional save card to checkout page in non-PCI solution

--- woocommerce-checkout-non-pci-gateway/woocommerce-checkout-non-pci.php
+++ woocommerce-checkout-non-pci-gateway/woocommerce-checkout-non-pci.php
@@ -302,6 +302,8 @@ class WC_Checkout_Non_Pci extends WC_Payment_Gateway {
 
         if($isHosted == 'yes' && $savedCard == self::PAYMENT_CARD_NEW_CARD){
 
+            $_SESSION['checkout_save_card_checked'] = isset($_POST['save-card-checkbox']);
+
             return array(
                 'result'        => 'success',
                 'redirect'      => $this->notify_url
@@ -383,7 +385,7 @@ class WC_Checkout_Non_Pci extends WC_Payment_Gateway {
         $woocommerce->cart->empty_cart();
 
         if (is_user_logged_in() && $this->saved_cards) {
-            WC_Checkout_Non_Pci_Customer_Card::saveCard($result, $order->user_id);
+            WC_Checkout_Non_Pci_Customer_Card::saveCard($result, $order->user_id, isset($_POST['save-card-checkbox']));
         }
 
         return array(
@@ -559,6 +561,8 @@ class WC_Checkout_Non_Pci extends WC_Payment_Gateway {
                 <input id="checkout-new-card" class="checkout-new-card-input" type="hidden" name="<?php echo $this->id . '-saved-card'?>" value="<?php echo self::PAYMENT_CARD_NEW_CARD?>"/>
             <?php endif?>
             <p class="form-row form-row-wide checkout-non-pci-new-card-row">
+                <input type="checkbox" name="save-card-checkbox" id="save-card-checkbox" value="1">
+                <label for="save-card-checkbox" style="position:relative; display:inline-block; margin-bottom: 10px; margin-top: 10px">Save card for future payments</label>
                 <?php if(!empty($paymentToken)):?>
                     <?php if($isPayOrder):?>
                         <input type="hidden" id="billing_email" value="<?php echo $billingEmail?>"/>
