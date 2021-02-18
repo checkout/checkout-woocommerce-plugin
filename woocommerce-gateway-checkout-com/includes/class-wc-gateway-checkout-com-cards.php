b8b78978394f6a898d12b349e3c4afc6f1a6c43b
ved-beejadhur-cko <ved.beejadhur@checkout.com>
2021-02-18 12:02:24 +0400

    ECPINT-2277: add validation to verify payment id when webhook is sent

--- woocommerce-gateway-checkout-com/includes/class-wc-gateway-checkout-com-cards.php
+++ woocommerce-gateway-checkout-com/includes/class-wc-gateway-checkout-com-cards.php
@@ -973,6 +973,13 @@ class WC_Gateway_Checkout_Com_Cards extends WC_Payment_Gateway_CC
         if($signature === false){
             return http_response_code(401);
         }
+
+        $payment_id = get_post_meta($data->data->metadata->order_id, '_cko_payment_id', true );
+
+        // check if payment ID matches that of the webhook
+        if($payment_id !== $data->data->id){
+            return http_response_code(422);
+        }
       
 
         // Get webhook event type from data
