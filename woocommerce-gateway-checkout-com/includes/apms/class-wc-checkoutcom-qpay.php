965098b0a2b429d55e8f5ef06fab0b4f5ebd928f
ved-beejadhur-cko <ved.beejadhur@checkout.com>
2021-03-18 17:17:49 +0400

    ECPINT-2418

--- woocommerce-gateway-checkout-com/includes/apms/class-wc-checkoutcom-qpay.php
+++ woocommerce-gateway-checkout-com/includes/apms/class-wc-checkoutcom-qpay.php
@@ -5,8 +5,6 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Qpay extends WC_Gateway_Check
     public function __construct()
     {
         $this->id = 'wc_checkout_com_alternative_payments_qpay';
-        $this->method_title = __("Checkout.com", 'wc_checkout_com');
-        $this->method_description = __("The Checkout.com extension allows shop owners to process online payments through the <a href=\"https://www.checkout.com\">Checkout.com Payment Gateway.</a>", 'wc_checkout_com');
         $this->title = __("QPay", 'wc_checkout_com');
         $this->has_fields = true;
         $this->supports = array('products', 'refunds');
@@ -20,12 +18,10 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Qpay extends WC_Gateway_Check
     {   
         // get available apms depending on currency
         $apm_available = WC_Checkoutcom_Utility::get_alternative_payment_methods();
+        $message = __("Pay with QPay. You will be redirected upon place order", 'wc_checkout_com')
 
         ?>
-            <script>
-                jQuery('.payment_box.payment_method_wc_checkout_com_alternative_payments_qpay').attr("style",
-                "visibility: hidden;");
-            </script>
+            <p style="margin-bottom: 0;"> <?php echo $message ?> </p>
             <input type="hidden" id="cko-apm" name="cko-apm" value="qpay">
         <?php
 
