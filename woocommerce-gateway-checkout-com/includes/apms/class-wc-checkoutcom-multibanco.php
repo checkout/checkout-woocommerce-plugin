0d17917f3aebaa5395a85b58e7c44ff7c78c1733
sanketchodavadiya <sanket18960@gmail.com>
2022-02-08 11:46:35 +0530

    Update payment method name

--- woocommerce-gateway-checkout-com/includes/apms/class-wc-checkoutcom-multibanco.php
+++ woocommerce-gateway-checkout-com/includes/apms/class-wc-checkoutcom-multibanco.php
@@ -21,7 +21,7 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway
 		$this->id                 = 'wc_checkout_com_alternative_payments_multibanco';
 		$this->method_title       = __( 'Checkout.com', 'wc_checkout_com' );
 		$this->method_description = __( 'The Checkout.com extension allows shop owners to process online payments through the <a href=\"https://www.checkout.com\">Checkout.com Payment Gateway.</a>', 'wc_checkout_com' );
-		$this->title              = __( 'Multibanco', 'wc_checkout_com' );
+		$this->title              = __( 'Pay by Multibanco with Checkout.com', 'wc_checkout_com' );
 		$this->supports           = array( 'products', 'refunds' );
 		$this->has_fields         = true;
 
@@ -36,11 +36,6 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway
 	public function payment_fields() {
 		// Get available apms depending on currency.
 		$apm_available = WC_Checkoutcom_Utility::get_alternative_payment_methods();
-		$message       = __( 'Pay with Multibanco', 'wc_checkout_com' );
-
-		?>
-        <p style="margin-bottom: 0;"> <?php echo $message ?> </p>
-		<?php
 
 		if ( ! in_array( self::PAYMENT_METHOD, $apm_available ) ) {
 			?>
