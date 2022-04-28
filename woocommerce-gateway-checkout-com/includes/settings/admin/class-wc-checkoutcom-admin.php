b15f6deba02d4d046f3880562296d0c8c7e33fcb
sanketchodavadiya <sanket18960@gmail.com>
2022-04-28 12:03:36 +0530

    Add functionality to highlight current settings page in menu

--- woocommerce-gateway-checkout-com/includes/settings/admin/class-wc-checkoutcom-admin.php
+++ woocommerce-gateway-checkout-com/includes/settings/admin/class-wc-checkoutcom-admin.php
@@ -1,67 +1,75 @@
 <?php
 
-class WC_Checkoutcom_Admin
-{
-    public static function generate_links($key, $value)
-    {
-        ?>
-        <div class="test" style="padding-bottom: 20px;    padding-top: 10px;">
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards' ); ?>"
-                       style="text-decoration: none"> <h2><?php _e( 'Core Settings', 'wc_checkout_com' );  ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=card_settings' ); ?>"
-                       style="text-decoration: none"><?php _e( 'Card Settings', 'wc_checkout_com' ); ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=orders_settings' ); ?>"
-                       style="text-decoration: none"><?php _e( 'Order Settings', 'wc_checkout_com' ); ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_google_pay' ); ?>"
-                       style="text-decoration: none"><?php _e( 'Google Pay', 'wc_checkout_com' ); ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_apple_pay' ); ?>"
-                       style="text-decoration: none"><?php _e( 'Apple Pay', 'wc_checkout_com' ); ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_alternative_payments' ); ?>"
-                       style="text-decoration: none"><?php _e( 'Alternative Payments', 'wc_checkout_com' ); ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=debug_settings' ); ?>"
-                       style="text-decoration: none"><?php _e( 'Debug Settings', 'wc_checkout_com' ); ?></a>
-                    <p style="display: unset;opacity: 0.5;"><?php echo '|'; ?></p>
-                </td>
-            </tr>
-            <tr valign="top">
-                <td colspan="2" class="forminp forminp-screen_button">
-                    <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=webhook' ); ?>"
-                            style="text-decoration: none"><?php _e( 'Webhook', 'wc_checkout_com' ); ?></a>
-                </td>
-            </tr>
-        </div>
-        <?php
-    }
+/**
+ * The admin-specific functionality of the plugin.
+ */
+class WC_Checkoutcom_Admin {
+
+	/**
+	 * Generate HTML links for the settings page.
+	 *
+	 * @param string $key   Key.
+	 * @param array  $value Value.
+	 *
+	 * @return void
+	 */
+	public static function generate_links( $key, $value ) {
+
+		$screen = ! empty( $_GET['screen'] ) ? sanitize_text_field( wp_unslash( $_GET['screen'] ) ) : '';
+		if ( empty( $screen ) ) {
+			$screen = ! empty( $_GET['section'] ) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : '';
+		}
+
+		?>
+		<style>
+			.cko-admin-settings__links {
+				padding-bottom: 20px;
+				padding-top: 10px;
+			}
+			.cko-admin-settings__links li {
+				color: #646970;
+				font-size: 17px;
+				font-weight: 600;
+				display: inline-block;
+				margin: 0;
+				padding: 0;
+				white-space: nowrap;
+			}
+			.cko-admin-settings__links a {
+				text-decoration: none;
+			}
+			.cko-admin-settings__links .current {
+				color: #000;
+			}
+		</style>
+		<div class="cko-admin-settings__links">
+			<ul>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards' ) ); ?>"
+						class="<?php echo 'wc_checkout_com_cards' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Core Settings', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=card_settings' ) ); ?>"
+						class="<?php echo 'card_settings' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Card Settings', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=orders_settings' ) ); ?>"
+						class="<?php echo 'orders_settings' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Order Settings', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_google_pay' ) ); ?>"
+						class="<?php echo 'wc_checkout_com_google_pay' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Google Pay', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_apple_pay' ) ); ?>"
+						class="<?php echo 'wc_checkout_com_apple_pay' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Apple Pay', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_alternative_payments' ) ); ?>"
+						class="<?php echo 'wc_checkout_com_alternative_payments' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Alternative Payments', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=debug_settings' ) ); ?>"
+						class="<?php echo 'debug_settings' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Debug Settings', 'wc_checkout_com' ); ?></a> | </li>
+				<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_checkout_com_cards&screen=webhook' ) ); ?>"
+						class="<?php echo 'webhook' === $screen ? 'current' : null; ?>">
+						<?php esc_html_e( 'Webhook', 'wc_checkout_com' ); ?></a></li>
+			</ul>
+		</div>
+		<?php
+	}
 }
