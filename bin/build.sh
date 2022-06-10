048136f329a70d12270b82ea1681465f7a0eb3a0
sanketchodavadiya <sanket18960@gmail.com>
2022-06-10 17:14:51 +0530

    Add GitHub action to build plugin zip on new release

---
+++ bin/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PLUGIN_SLUG="checkout-com-unified-payments-api"
+
+echo "Installing PHP and JS dependencies..."
+composer install --no-dev || exit "$?"
+
+echo "Create build directory..."
+mkdir "$PLUGIN_SLUG"
+
+echo "Copy plugin data to build folder..."
+cp -R "assets" "$PLUGIN_SLUG/assets/"
+cp -R "includes" "$PLUGIN_SLUG/includes/"
+cp -R "languages" "$PLUGIN_SLUG/languages/"
+cp -R "lib" "$PLUGIN_SLUG/lib/"
+cp -R "vendor" "$PLUGIN_SLUG/vendor/"
+cp -R "templates" "$PLUGIN_SLUG/templates/"
+cp "readme.txt" "$PLUGIN_SLUG/readme.txt"
+cp "woocommerce-gateway-checkout-com.php" "$PLUGIN_SLUG/woocommerce-gateway-checkout-com.php"
+
+echo "Generating zip file..."
+zip -q -r "${PLUGIN_SLUG}.zip" "$PLUGIN_SLUG/"
+
+echo "${PLUGIN_SLUG}.zip file generated!"
+echo "Build done!"
