27678171cf8f9adc9e1cfdff2776cf375055fe17
Souptik Datta <souptikdatta2001@gmail.com>
2022-06-16 20:04:55 +0530

    fix: Fix multibanco apm file phpcs issues

--- includes/apms/class-wc-checkoutcom-multibanco.php
+++ includes/apms/class-wc-checkoutcom-multibanco.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * Multibanco APM class.
+ *
+ * @package wc_checkout_com
+ */
 
 /**
  * Multibanco payment method class.
  *
+ * @class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco
  * @extends WC_Gateway_Checkout_Com_Alternative_Payments
  */
 class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway_Checkout_Com_Alternative_Payments {
@@ -14,20 +20,20 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway
 	 */
 	const PAYMENT_METHOD = 'multibanco';
 
-    /**
-     * Constructor.
-     */
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		$this->id                 = 'wc_checkout_com_alternative_payments_multibanco';
 		$this->method_title       = __( 'Checkout.com', 'wc_checkout_com' );
 		$this->method_description = __( 'The Checkout.com extension allows shop owners to process online payments through the <a href=\"https://www.checkout.com\">Checkout.com Payment Gateway.</a>', 'wc_checkout_com' );
 		$this->title              = __( 'Pay by Multibanco with Checkout.com', 'wc_checkout_com' );
-		$this->supports           = array( 'products' );
+		$this->supports           = [ 'products' ];
 		$this->has_fields         = true;
 
 		$this->init_form_fields();
 
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, [ $this, 'process_admin_options' ] );
 	}
 
 	/**
@@ -37,27 +43,26 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway
 		// Get available apms depending on currency.
 		$apm_available = WC_Checkoutcom_Utility::get_alternative_payment_methods();
 
-		if ( ! in_array( self::PAYMENT_METHOD, $apm_available ) ) {
+		if ( ! in_array( self::PAYMENT_METHOD, $apm_available, true ) ) {
 			?>
-            <script>
+			<script>
 				jQuery( '.payment_method_wc_checkout_com_alternative_payments_multibanco' ).hide();
-            </script>
+			</script>
 			<?php
 		}
 	}
 
 	/**
-	 * Process the payment and return the result.
+	 * Process Multibanco APM payment.
 	 *
 	 * @param  int $order_id Order ID.
+	 *
 	 * @return array
 	 */
 	public function process_payment( $order_id ) {
 		if ( ! session_id() ) {
 			session_start();
 		}
-
-		global $woocommerce;
 
 		$order = wc_get_order( $order_id );
 
@@ -66,7 +71,7 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway
 
 		// Check if result has error and return error message.
 		if ( isset( $result['error'] ) && ! empty( $result['error'] ) ) {
-			WC_Checkoutcom_Utility::wc_add_notice_self( __( $result['error'] ), 'error' );
+			WC_Checkoutcom_Utility::wc_add_notice_self( $result['error'], 'error' );
 
 			return;
 		}
@@ -74,10 +79,10 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Multibanco extends WC_Gateway
 		// Redirect to apm if redirection url is available.
 		if ( isset( $result['apm_redirection'] ) && ! empty( $result['apm_redirection'] ) ) {
 
-			return array(
+			return [
 				'result'   => 'success',
 				'redirect' => $result['apm_redirection'],
-			);
+			];
 		}
 	}
 
