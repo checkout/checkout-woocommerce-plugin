31395cb42f7f36395a0475dcded8c4ef232aaf12
Souptik Datta <souptikdatta2001@gmail.com>
2022-06-16 20:20:46 +0530

    fix: Fix sofort apm file phpcs issues

--- includes/apms/class-wc-checkoutcom-sofort.php
+++ includes/apms/class-wc-checkoutcom-sofort.php
@@ -1,68 +1,88 @@
 <?php
+/**
+ * Sofort APM class.
+ *
+ * @package wc_checkout_com
+ */
 
+/**
+ * Class WC_Gateway_Checkout_Com_Alternative_Payments_Sofort
+ *
+ * @class   WC_Gateway_Checkout_Com_Alternative_Payments_Sofort
+ * @extends WC_Gateway_Checkout_Com_Alternative_Payments
+ */
 class WC_Gateway_Checkout_Com_Alternative_Payments_Sofort extends WC_Gateway_Checkout_Com_Alternative_Payments {
 
-    const PAYMENT_METHOD = 'sofort';
+	const PAYMENT_METHOD = 'sofort';
 
-    public function __construct()
-    {
-        $this->id = 'wc_checkout_com_alternative_payments_sofort';
-        $this->title = __("Sofort", 'wc_checkout_com');
-        $this->has_fields = true;
-        $this->supports = array('products', 'refunds');
+	/**
+	 * Construct method.
+	 */
+	public function __construct() {
+		$this->id         = 'wc_checkout_com_alternative_payments_sofort';
+		$this->title      = __( 'Sofort', 'wc_checkout_com' );
+		$this->has_fields = true;
+		$this->supports   = [ 'products', 'refunds' ];
 
-        $this->init_form_fields();
+		$this->init_form_fields();
 
-        add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
-    }
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, [ $this, 'process_admin_options' ] );
+	}
 
-    public function payment_fields()
-    {
+	/**
+	 * Payment fields to be displayed.
+	 */
+	public function payment_fields() {
+		// get available apms depending on currency.
+		$apm_available = WC_Checkoutcom_Utility::get_alternative_payment_methods();
+		$message       = __( 'Pay with Sofort. You will be redirected upon place order', 'wc_checkout_com' );
 
-        // get available apms depending on currency
-        $apm_available = WC_Checkoutcom_Utility::get_alternative_payment_methods();
-        $message = __("Pay with Sofort. You will be redirected upon place order", 'wc_checkout_com');
+		?>
+			<p style="margin-bottom: 0;"> <?php echo $message; ?> </p>
+		<?php
 
-        ?>
-            <p style="margin-bottom: 0;"> <?php echo $message ?> </p>
-        <?php
+		if ( ! in_array( self::PAYMENT_METHOD, $apm_available, true ) ) {
+			?>
+				<script>
+					jQuery('.payment_method_wc_checkout_com_alternative_payments_sofort').hide();
+				</script>
+			<?php
+		}
 
-        if (! in_array(self::PAYMENT_METHOD, $apm_available) ) {
-            ?>
-                <script>
-                    jQuery('.payment_method_wc_checkout_com_alternative_payments_sofort').hide();
-                </script>
-            <?php
-        }
+	}
 
-    }
+	/**
+	 * Process Sofort APM payment.
+	 *
+	 * @param int $order_id Order ID.
+	 *
+	 * @return array
+	 */
+	public function process_payment( $order_id ) {
+		if ( ! session_id() ) {
+			session_start();
+		}
 
-    public function process_payment( $order_id )
-    {
-        if (!session_id()) session_start();
+		$order = wc_get_order( $order_id );
 
-        global $woocommerce;
+		// create alternative payment.
+		$result = (array) WC_Checkoutcom_Api_request::create_apm_payment( $order, self::PAYMENT_METHOD );
 
-        $order = wc_get_order( $order_id );
+		// check if result has error and return error message.
+		if ( isset( $result['error'] ) && ! empty( $result['error'] ) ) {
+			WC_Checkoutcom_Utility::wc_add_notice_self( $result['error'], 'error' );
+			return;
+		}
 
-        // create alternative payment
-        $result =  (array) WC_Checkoutcom_Api_request::create_apm_payment($order, self::PAYMENT_METHOD);
+		// redirect to apm if redirection url is available.
+		if ( isset( $result['apm_redirection'] ) && ! empty( $result['apm_redirection'] ) ) {
 
-        // check if result has error and return error message
-        if (isset($result['error']) && !empty($result['error'])) {
-            WC_Checkoutcom_Utility::wc_add_notice_self(__($result['error']), 'error');
-            return;
-        }
-
-        // redirect to apm if redirection url is available
-        if (isset($result['apm_redirection']) &&!empty($result['apm_redirection'])) {
-
-            return array(
-                'result'        => 'success',
-                'redirect'      => $result['apm_redirection'],
-            );
-        }
-    }
+			return [
+				'result'   => 'success',
+				'redirect' => $result['apm_redirection'],
+			];
+		}
+	}
 
 	/**
 	 * Process refund for the order.
@@ -76,5 +96,6 @@ class WC_Gateway_Checkout_Com_Alternative_Payments_Sofort extends WC_Gateway_Che
 	public function process_refund( $order_id, $amount = null, $reason = '' ) {
 
 		return parent::process_refund( $order_id, $amount, $reason );
+
 	}
 }
