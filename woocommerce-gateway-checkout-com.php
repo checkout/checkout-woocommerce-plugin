3c5b0d1a5c0427d58fbffb37ab9fa7a1f59c0b68
Sanket Chodavadiya <21277725+SanketChodavadiya@users.noreply.github.com>
2024-01-17 18:52:54 +0530

    update: version to 4.4.19 and changelog (#366)

--- woocommerce-gateway-checkout-com.php
+++ woocommerce-gateway-checkout-com.php
@@ -5,9 +5,9 @@
  * Description: Extends WooCommerce by Adding the Checkout.com Gateway.
  * Author: Checkout.com
  * Author URI: https://www.checkout.com/
- * Version: 4.4.18
+ * Version: 4.4.19
  * Requires at least: 5.0
- * Stable tag: 4.4.18
+ * Stable tag: 4.4.19
  * Tested up to: 6.4.1
  * WC tested up to: 8.3.1
  * Requires PHP: 7.3
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Constants.
  */
-define( 'WC_CHECKOUTCOM_PLUGIN_VERSION', '4.4.18' );
+define( 'WC_CHECKOUTCOM_PLUGIN_VERSION', '4.4.19' );
 define( 'WC_CHECKOUTCOM_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 define( 'WC_CHECKOUTCOM_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
